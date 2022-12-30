7108a9903558358e7e9fbdbbcc21daeb3eb3558c
sivaraya01 <58845939+sivaraya01@users.noreply.github.com>
2022-12-30 09:46:01 +0530

    new files added
    add all files

---
+++ EmployeeApplication/src/main/java/com/example/EmployeeApplication/controller/EmployeeController.java
@@ -0,0 +1,40 @@
+package com.example.EmployeeApplication.controller;
+
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.EmployeeApplication.model.Employee;
+
+@RestController
+public class EmployeeController {
+	
+	@GetMapping("/getEmployee")
+	public Employee getEmployeeDetails() {
+		System.out.println("GET Employee Method Started....");
+		
+		Employee employee=new Employee();
+		employee.setEmployeeId(123);
+		employee.setName("Siva");
+		employee.setCompany("Virtusa");
+		employee.setLocation("Bangalore");
+		
+		System.out.println("Employee name is :: "+employee.getName());//Employee name is :: Siva
+		
+		return employee;
+	}
+	
+	@PostMapping(path="/saveEmployee",consumes = MediaType.APPLICATION_JSON_VALUE)
+	public Object saveEmployeeDetails(@RequestBody Employee employee) {
+		
+		System.out.println("Employee Details....!");
+		System.out.println(employee.getEmployeeId());
+		System.out.println(employee.getName());
+		System.out.println(employee.getCompany());
+		System.out.println(employee.getLocation());
+		return "Employee Details Saved Successfully..";
+	}
+	
+}
---
+++ EmployeeApplication/src/main/java/com/example/EmployeeApplication/model/Employee.java
@@ -0,0 +1,37 @@
+package com.example.EmployeeApplication.model;
+
+public class Employee {
+	
+	private int employeeId;
+	private String name;
+	private String company;
+	private String location;
+	
+	public int getEmployeeId() {
+		return employeeId;
+	}
+	public void setEmployeeId(int employeeId) {
+		this.employeeId = employeeId;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getCompany() {
+		return company;
+	}
+	public void setCompany(String company) {
+		this.company = company;
+	}
+	public String getLocation() {
+		return location;
+	}
+	public void setLocation(String location) {
+		this.location = location;
+	}
+	
+	
+
+}
