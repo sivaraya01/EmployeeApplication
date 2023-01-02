79e420047e3def0c6201abd8d90aef0f20da8b52
sivaraya01 <58845939+sivaraya01@users.noreply.github.com>
2023-01-02 19:08:01 +0530

    Create StudentInformation.java
    added logs from studentInformation

---
+++ src/main/java/com/example/EmployeeApplication/model/StudentInformation.java
@@ -0,0 +1,39 @@
+package com.example.EmployeeApplication.model;
+
+public class StudentInformation{
+  private int studentRollNO;
+  private String Name;
+  private String Stream;
+  private String Address;
+public int getStudentRollNO() {
+	return studentRollNO;
+}
+public void setStudentRollNO(int studentRollNO) {
+	this.studentRollNO = studentRollNO;
+}
+public String getName() {
+	return Name;
+}
+public void setName(String name) {
+	Name = name;
+}
+public String getStream() {
+	return Stream;
+}
+public void setStream(String stream) {
+	Stream = stream;
+}
+public String getAddress() {
+	return Address;
+}
+public void setAddress(String address) {
+	Address = address;
+
+}
+	
+}
+	
+
+	
+
+
