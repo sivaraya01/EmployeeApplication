e9bae1db009398fc75d65d1522a25520263d4f28
sivaraya01 <siva.rayapureddy@gmail.com>
2023-01-04 09:49:06 +0530

    Employee details saved into DB

--- src/main/java/com/example/EmployeeApplication/controller/EmployeeController.java
+++ src/main/java/com/example/EmployeeApplication/controller/EmployeeController.java
@@ -1,5 +1,6 @@
 package com.example.EmployeeApplication.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -8,27 +9,30 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.EmployeeApplication.model.Employee;
+import com.example.EmployeeApplication.repository.EmployeeRepository;
 
 @RestController
 public class EmployeeController {
 	
-//	@Autowired
-//	EmployeeRepository employeeRepository;
+	@Autowired
+	EmployeeRepository employeeRepository;
 	
 	@GetMapping("/getEmployee/{id}")
 	public Employee getEmployeeDetails(@PathVariable int id) {
 		System.out.println("GET Employee Method Started....");
 		
-	//	Optional<Employee> employeeData=employeeRepository.findById(id);
-		//Employee employee=employeeData.get();
-	//	System.out.println("Employee name is :: "+employee.getName());//Employee name is :: Siva
-		
-		//System.out.println("Employee name is :: "+employee.getName());
-		
-		//return employee;
-	//	return employee;
-		return null;
+		Employee employee=employeeRepository.getById(id);
+	
+		return employee;
 	}
+	
+	
+	
+	
+	
+	
+	
+	
 	@PostMapping(path="/saveEmployee",consumes = MediaType.APPLICATION_JSON_VALUE)
 	public Object saveEmployeeDetails(@RequestBody Employee employee) {
 		
@@ -37,7 +41,7 @@ public class EmployeeController {
 		System.out.println("employee name::" +employee.getName());
 		System.out.println("employee company ::"+employee.getCompany());
 		System.out.println(employee.getLocation());
-	//	employeeRepository.save(employee);
+		employeeRepository.save(employee);
 		return "Employee Details Saved Successfully..";
 	}
 	
--- src/main/java/com/example/EmployeeApplication/repository/EmployeeRepository.java
+++ src/main/java/com/example/EmployeeApplication/repository/EmployeeRepository.java
@@ -6,6 +6,6 @@ import org.springframework.stereotype.Repository;
 import com.example.EmployeeApplication.model.Employee;
 
 @Repository
-public interface EmployeeRepository {//extends JpaRepository<Employee, Integer>{
+public interface EmployeeRepository extends JpaRepository<Employee, Integer>{
 
 }
