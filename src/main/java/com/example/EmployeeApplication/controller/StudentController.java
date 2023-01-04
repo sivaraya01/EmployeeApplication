a8a05100b8eb8dbcdc7df5e9e8df08a9600a8f3c
sivaraya01 <58845939+sivaraya01@users.noreply.github.com>
2023-01-04 23:10:38 +0530

    Update StudentController.java
    add logs studentcontroller

--- src/main/java/com/example/EmployeeApplication/controller/StudentController.java
+++ src/main/java/com/example/EmployeeApplication/controller/StudentController.java
@@ -1,31 +1,43 @@
 package com.example.EmployeeApplication.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.repository.Repository;
+import org.springframework.data.repository.RepositoryDefinition;
+import org.springframework.data.repository.config.RepositoryConfigurationUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.EmployeeApplication.model.StudentInformation;
+import com.example.EmployeeApplication.repository.StudentInformationRepository;
 
 @RestController
 
 public class StudentController {
 	
-@GetMapping(path="/getStudentrecord",produces="application/json")	
+	@Autowired
+	
+	StudentInformationRepository studentInformationRepository;
+
+	
+//@GetMapping(path="/getStudentrecord",produces="application/json")	
+	@GetMapping("/getStudentRecord/{id}")
     public StudentInformation getStudentInformation() {
 	
 		System.out.println("get student record.......");
 		
-		
-		StudentInformation student = new StudentInformation();
-		student.setStudentRollNO(14391);
-		student.setName("raj");
-		student.setStream("computers");
-		student.setAddress("kerala");
+	//	Student student = studentinformation
+	//	StudentInformation student = new StudentInformation();
+	//	student.setStudentRollNO(14391);
+	//	student.setName("raj");
+	//	student.setStream("computers");
+	//	student.setAddress("kerala");
 		  
-		System.out.println("student name is ::"+student.getName());
+	//	System.out.println("student name is ::"+student.getName());
 		
-		return student;
+	//	return student;
+		return null;
 }
 
 	
@@ -42,7 +54,9 @@ public Object saveStudentInformation(@RequestBody StudentInformation student) {
   System.out.println("student Name::"+ student.getName());
   System.out.println("student Stream::"+ student.getStream());
   System.out.println("student Address::"+ student.getAddress());
-   
+
+  //StudentInformationRepository.save (student);
+  studentInformationRepository.save(student);
   return "student Information successfully added.....";
 }
   
